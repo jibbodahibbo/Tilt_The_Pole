64cc3798eec8a2efe863a7ba555b7f2061350b8e
Jim <jtwesterkamp@gmail.com>
2018-10-31 18:18:35 -0500

    Added DB4

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,5 +1,5 @@
 <?php
- $url = parse_url(getenv("mysql://b0d41420cfca89:92b34fca@us-cdbr-iron-east-01.cleardb.net/heroku_c489e6d16d0d210?reconnect=true"));
+ $url = parse_url("mysql://b0d41420cfca89:92b34fca@us-cdbr-iron-east-01.cleardb.net/heroku_c489e6d16d0d210?reconnect=true");
 define("DB_SERVER", $url["host"]);
 define("DB_USER", $url["user"]);
 define("DB_PASS", $url["pass"]);
