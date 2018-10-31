408fe50cc8868be693786017156421eca4ea1702
Jim <jtwesterkamp@gmail.com>
2018-10-31 18:41:21 -0500

    Added Dug

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,9 +1,9 @@
 <?php
  $url = parse_url("mysql://b0d41420cfca89:92b34fca@us-cdbr-iron-east-01.cleardb.net/heroku_c489e6d16d0d210?reconnect=true");
-define("DB_SERVER", $url["host"]);
-define("DB_USER", $url["user"]);
-define("DB_PASS", $url["pass"]);
-define("DB_NAME", substr($url["path"], 1));
+define("DB_SERVER", "us-cdbr-iron-east-01");
+define("DB_USER", "b0d41420cfca89");
+define("DB_PASS", "92b34fca");
+define("DB_NAME", "heroku_c489e6d16d0d210");
 
 
 
