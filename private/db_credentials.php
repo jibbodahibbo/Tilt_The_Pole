ff89dcfed3340587f6276e5093ec22b956ce71b0
Jim <jtwesterkamp@gmail.com>
2018-10-31 18:01:51 -0500

    Added DB Confi32

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,8 +1,11 @@
 <?php
-define("DB_SERVER", "us-cdbr-iron-east-01.cleardb.net");
-define("DB_USER", "bd0da8fb4f204d");
-define("DB_PASS", "db2cf689");
-define("DB_NAME", "heroku_942adfaec1d5dac");
+ $url = parse_url(getenv("mysql://b0d41420cfca89:92b34fca@us-cdbr-iron-east-01.cleardb.net/heroku_c489e6d16d0d210?reconnect=true"));
+define("DB_SERVER", $url["host"]);
+define("DB_USER", $url["user"]);
+define("DB_PASS", $url["pass"]);
+define("DB_NAME", substr($url["path"], 1);
+
+
 
 
  ?>
