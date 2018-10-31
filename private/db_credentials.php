34cd5dd336bccf0243f6bfd64a5ad0a313c8f779
Jim <jtwesterkamp@gmail.com>
2018-10-31 17:49:23 -0500

    Added DB Config

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,8 +1,9 @@
 <?php
-define("DB_SERVER", "localhost");
-define("DB_USER", "blogger");
-define("DB_PASS", "bendthestick25");
-define("DB_NAME", "blog_db");
+define("DB_SERVER", "us-cdbr-iron-east-01.cleardb.net");
+define("DB_USER", "bd0da8fb4f204d");
+define("DB_PASS", "db2cf689");
+define("DB_NAME", "heroku_942adfaec1d5dac");
 
 
  ?>
+mysql://bd0da8fb4f204d:db2cf689@us-cdbr-iron-east-01.cleardb.net/heroku_942adfaec1d5dac?reconnect=true
