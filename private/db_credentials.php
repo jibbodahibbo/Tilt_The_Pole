92013dfb443b0b1758d39907d05f3c319ab95844
Jim <jtwesterkamp@gmail.com>
2018-10-31 18:56:06 -0500

    Added 98787

--- private/db_credentials.php
+++ private/db_credentials.php
@@ -1,6 +1,6 @@
 <?php
  $url = parse_url("mysql://b0d41420cfca89:92b34fca@us-cdbr-iron-east-01.cleardb.net/heroku_c489e6d16d0d210?reconnect=true");
-define("DB_SERVER", "us-cdbr-iron-east-01");
+define("DB_SERVER", "us-cdbr-iron-east-01.cleardb.net");
 define("DB_USER", "b0d41420cfca89");
 define("DB_PASS", "92b34fca");
 define("DB_NAME", "heroku_c489e6d16d0d210");
