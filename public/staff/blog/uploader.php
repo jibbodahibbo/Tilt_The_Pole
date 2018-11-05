48444280dcdf8857f91e5a3ff88d5ed17d8ae862
Jim <jtwesterkamp@gmail.com>
2018-11-04 18:58:28 -0600

    new five

--- public/staff/blog/uploader.php
+++ public/staff/blog/uploader.php
@@ -40,7 +40,7 @@
             <li>File size: <?php echo $_FILES['image']['size'];  ?>
             <li>File type: <?php echo $_FILES['image']['type'] ?>
 
-      <li>    <?php echo url_for( $_FILES['image']['name'] )?> </li>
+        <li>    <?php echo PUBLIC_PATH . '/staff/blog/images/' . $_FILES['image']['name'] ?> </li>
          </ul>
 
       </form>
