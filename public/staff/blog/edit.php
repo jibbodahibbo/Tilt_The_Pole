c2413257be5662c53142c3df27b688c25109d529
Jim <jtwesterkamp@gmail.com>
2018-11-06 21:37:59 -0600

    img uploadin2

--- public/staff/blog/edit.php
+++ public/staff/blog/edit.php
@@ -6,42 +6,7 @@ Protect\with('form.php', 'bendthestick');
 <?php $currentpage="blog"?>
 <?php  include(SHARED_PATH . '/header.php') ?>
 
-<?php
 
-   if(isset($_FILES['image'])){
-      $errors= array();
-      $file_name = $_FILES['image']['name'];
-      $file_size = $_FILES['image']['size'];
-      $file_tmp = $_FILES['image']['tmp_name'];
-      $file_type = $_FILES['image']['type'];
-      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
-
-      $extensions= array("jpeg","jpg","png");
-
-      if(in_array($file_ext,$extensions)=== false){
-         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-      }
-
-      if($file_size > 2097152) {
-         $errors[]='File size must be less that 2 MB';
-      }
-
-      if(empty($errors)==true) {
-        $directory = "images/blog/";
-        $filecount = 0;
-        $files = glob($directory . "*");
-        if ($files){
-         $filecount = count($files);
-        }
-        $a = "$filecount";
-         move_uploaded_file($file_tmp,"images/blog/". $a . $file_name );
-         copy("images/blog/".$a.$file_name, PUBLIC_PATH ."/images/blog/". $a . $file_name );
-      }else{
-         print_r($errors);
-      }
-   }
-
-?>
 <!--------Header-->
 
 <?php
@@ -49,24 +14,68 @@ $blog_post = find_subject_by_id();
 ?>
 
 <head>
-  <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=your_API_key"></script>
+  <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=7oizjx5am2cxkdjd242iwu4hpyfsi547xfglnl2ould24x16"></script>
 
-<script type="text/javascript">
-tinymce.init({
-selector: 'textarea',
-height: 500,
-menubar: false,
-plugins: [
-'advlist autolink lists link image charmap print preview anchor textcolor',
-'searchreplace visualblocks code fullscreen',
-'insertdatetime media table contextmenu paste code help wordcount'
-],
-toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
-content_css: [
-'//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
-'//www.tinymce.com/css/codepen.min.css']
-});
-</script>
+  <script type="text/javascript">
+  tinymce.init({
+  selector: 'textarea',
+  height: 500,
+  menubar: false,
+  plugins: [
+    'advlist autolink lists link image charmap print preview anchor textcolor',
+    'searchreplace visualblocks code fullscreen',
+    'insertdatetime media table contextmenu paste code help wordcount'
+  ],
+  toolbar: 'insert | undo redo |  formatselect | bold italic backcolor  | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help',
+  content_css: [
+    '//fonts.googleapis.com/css?family=Lato:300,300i,400,400i',
+    '//www.tinymce.com/css/codepen.min.css'],
+    image_title: true,
+      // enable automatic uploads of images represented by blob or data URIs
+      automatic_uploads: true,
+      // URL of our upload handler (for more details check: https://www.tinymce.com/docs/configure/file-image-upload/#images_upload_url)
+      // images_upload_url: 'postAcceptor.php',
+      // here we add custom filepicker only to Image dialog
+      file_picker_types: 'image',
+      // and here's our custom image picker
+      file_picker_callback: function(cb, value, meta) {
+        var input = document.createElement('input');
+        input.setAttribute('type', 'file');
+        input.setAttribute('accept', 'image/*');
+
+        // Note: In modern browsers input[type="file"] is functional without
+        // even adding it to the DOM, but that might not be the case in some older
+        // or quirky browsers like IE, so you might want to add it to the DOM
+        // just in case, and visually hide it. And do not forget do remove it
+        // once you do not need it anymore.
+
+        input.onchange = function() {
+          var file = this.files[0];
+
+          var reader = new FileReader();
+          reader.onload = function () {
+            // Note: Now we need to register the blob in TinyMCEs image blob
+            // registry. In the next release this part hopefully won't be
+            // necessary, as we are looking to handle it internally.
+            var id = 'blobid' + (new Date()).getTime();
+            var blobCache =  tinymce.activeEditor.editorUpload.blobCache;
+            var base64 = reader.result.split(',')[1];
+            var blobInfo = blobCache.create(id, file, base64);
+            blobCache.add(blobInfo);
+
+            // call the callback and populate the Title field with the file name
+            cb(blobInfo.blobUri(), { title: file.name });
+          };
+          reader.readAsDataURL(file);
+        };
+
+        input.click();
+      }
+  });
+
+
+
+  </script>
 </head>
 
 
--- public/staff/blog/new.php
+++ public/staff/blog/new.php
@@ -6,47 +6,12 @@ Protect\with('form.php', 'bendthestick');
 <?php $currentpage="blog" ?>
 <?php  include(SHARED_PATH . '/header.php') ?>
 
-<?php
 
-   if(isset($_FILES['image'])){
-      $errors= array();
-      $file_name = $_FILES['image']['name'];
-      $file_size = $_FILES['image']['size'];
-      $file_tmp = $_FILES['image']['tmp_name'];
-      $file_type = $_FILES['image']['type'];
-      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
-
-      $extensions= array("jpeg","jpg","png");
-
-      if(in_array($file_ext,$extensions)=== false){
-         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
-      }
-
-      if($file_size > 2097152) {
-         $errors[]='File size must be less that 2 MB';
-      }
-
-      if(empty($errors)==true) {
-        $directory = "images/blog/";
-        $filecount = 0;
-        $files = glob($directory . "*");
-        if ($files){
-         $filecount = count($files);
-        }
-        $a = "$filecount";
-         move_uploaded_file($file_tmp,"images/blog/". $a . $file_name );
-         copy("images/blog/".$a.$file_name, PUBLIC_PATH ."/images/blog/". $a . $file_name );
-      }else{
-         print_r($errors);
-      }
-   }
-
-?>
 
 
   <!--------Header-->
   <head>
-    <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=your_API_key"></script>
+    <script src="https://cloud.tinymce.com/stable/tinymce.min.js?apiKey=7oizjx5am2cxkdjd242iwu4hpyfsi547xfglnl2ould24x16"></script>
 
 <script type="text/javascript">
 tinymce.init({
