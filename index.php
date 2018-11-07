430ffecbdc484feb645b46749dfb519728e3769e
Jim <jtwesterkamp@gmail.com>
2018-11-07 09:32:11 -0600

    mailer

--- index.php
+++ index.php
@@ -1,3 +1,13 @@
 <?php echo "this is a test"; ?>
 
 This is a text
+
+<?php
+$inipath = php_ini_loaded_file();
+
+if ($inipath) {
+    echo 'Loaded php.ini: ' . $inipath;
+} else {
+    echo 'A php.ini file is not loaded';
+}
+?>
--- public/send_message.php
+++ public/send_message.php
@@ -1,15 +1,17 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//Load Composer's autoloader
+require '../vendor/autoload.php';
+
+ini_set('display_errors',1);
 if(isset($_POST['email'])) {
 
     // EDIT THE 2 LINES BELOW AS REQUIRED
     $email_to = "jtwesterkamp@gmail.com";
+    $email_subject = "Your email subject line";
 
-    $subject = isset($_GET['id']) ? $_GET['id']: '';
-    if ($subject =='res'){
-    $email_subject = "Residential Customer Message";
-    }else{
-    $email_subject = "Commercial Customer Message";
-    }
     function died($error) {
         // your error code can go here
         echo "We are very sorry, but there were error(s) found with the form you submitted. ";
@@ -22,34 +24,31 @@ if(isset($_POST['email'])) {
 
     // validation expected data exists
     if(!isset($_POST['name']) ||
-        !isset($_POST['email']) ||
-        !isset($_POST['message']))
-        {
+        !isset($_POST['message']) ||
+        !isset($_POST['email'])) {
         died('We are sorry, but there appears to be a problem with the form you submitted.');
     }
 
 
 
-    $name = $_POST['name']; // required
-    $email = $_POST['email']; // required
-    $message = $_POST['message']; // required
-
+    $first_name = $_POST['name']; // required
+    $email_from = $_POST['email']; // required
+    $comments = $_POST['message']; // required
 
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
 
-  if(!preg_match($email_exp,$email)) {
+  if(!preg_match($email_exp,$email_from)) {
     $error_message .= 'The Email Address you entered does not appear to be valid.<br />';
   }
 
     $string_exp = "/^[A-Za-z .'-]+$/";
 
-  if(!preg_match($string_exp,$name)) {
+  if(!preg_match($string_exp,$first_name)) {
     $error_message .= 'The First Name you entered does not appear to be valid.<br />';
   }
 
-
-  if(strlen($message) < 2) {
+  if(strlen($comments) < 2) {
     $error_message .= 'The Comments you entered do not appear to be valid.<br />';
   }
 
@@ -67,28 +66,49 @@ if(isset($_POST['email'])) {
 
 
 
-    $email_message .= "Name: ".clean_string($name)."\n";
-    $email_message .= "Email: ".clean_string($email)."\n";
-    $email_message .= "Message: ".clean_string($message)."\n";
-
-// create email headers
-$headers = 'From: '.$email."\r\n".
-'Reply-To: '.$email."\r\n" .
-'X-Mailer: PHP/' . phpversion();
-@mail($email_to, $email_subject, $email_message, $headers);
-
-require_once('../private/initialize.php');
-$currentpage="contact";
-include(SHARED_PATH . '/header.php');
-
-redirect_to(url_for('/contact.php?sent=true'));
-?>
-
-<!-- include your own success html here -->
+    $email_message .= "Name: ".clean_string($first_name)."\n";
+    $email_message .= "Email: ".clean_string($email_from)."\n";
+    $email_message .= "Comments: ".clean_string($comments)."\n";
 
 
 
-<?php
+    // Import PHPMailer classes into the global namespace
+    // These must be at the top of your script, not inside a function
 
-}
+
+    $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
+    try {
+        //Server settings
+        $mail->SMTPDebug = 2;                                 // Enable verbose debug output
+        $mail->isSMTP();                                      // Set mailer to use SMTP
+        $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
+        $mail->SMTPAuth = true;                               // Enable SMTP authentication
+        $mail->Username = $email_to;                 // SMTP username
+        $mail->Password = 'yeuqjrmkluumedgj';                           // SMTP password
+        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
+        $mail->Port = 587;                                    // TCP port to connect to
+
+        //Recipients
+        $mail->setFrom($email_from, 'Mailer');
+        $mail->addAddress($email_to);     // Add a recipient
+        $mail->addReplyTo($email_from, 'Information');
+
+
+        //Content
+        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->Subject = $email_subject;
+        $MAIL->Body = "Form details below.\n\n";
+        $mail->Body .= "Name: ".clean_string($first_name)."\n";
+        $mail->Body .= "Email: ".clean_string($email_from)."\n";
+        $mail->Body .= "Message: ".clean_string($comments)."\n";
+        $mail->AltBody = $first_name .' '. $email_from .' '. $comments;
+
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+    }
+
+  }
 ?>
