7ee2f3c41634daa1a468efd0e3b0e07d72a5fd48
Jim <jtwesterkamp@gmail.com>
2018-12-28 11:19:09 -0600

    adding share buttons

--- public/blog.php
+++ public/blog.php
@@ -36,6 +36,20 @@ $x+=1;   ?>
   <?php echo $blog_post['content']; ?>
 </div>
 
+<!--- Social Share  -->
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v3.2';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+
+<div class="fb-share-button" data-href="<?php echo WWW_ROOT . "/blogpost.php?id=" . h(u($blog_post['id'])); ?>" data-layout="button" data-size="small" data-mobile-iframe="true"><a target="_blank" href=" <?php echo "https://www.facebook.com/sharer/sharer.php?u=" . WWW_ROOT . "/blogpost.php?id=" . h(u($blog_post['id'])); ?>" class="fb-xfbml-parse-ignore">Share</a></div>
+<!--- Social Share END-->
+<br>
 <small> <?php echo h($blog_post['date']); ?></small>
 </div>
 
@@ -45,8 +59,9 @@ $x+=1;   ?>
 <br><br><br>
 <hr>
 <?php }?>
-
 <br>
+
+
 
 <?php
 mysqli_free_result($blogposts);
