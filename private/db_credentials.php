39652b3ffb5c9d097e7caf1e8970964091721679
jibbodahibbo <jtwesterkamp@gmail.com>
2021-09-25 16:37:49 -0500

    Delete db_credentials.php

--- private/db_credentials.php
+++
@@ -1,7 +0,0 @@
-<?php
- $url = parse_url("mysql://b0d41420cfca89:92b34fca@us-cdbr-iron-east-01.cleardb.net/heroku_c489e6d16d0d210?reconnect=true");
-define("DB_SERVER", "snailmemescom.fatcowmysql.com");
-define("DB_USER", "ttp");
-define("DB_PASS", "ttp123abc");
-define("DB_NAME", "blog_posts");
- ?>
